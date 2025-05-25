30e1b3a8b85ad87cd488c565844efb50ab989412
Kristoffer Larsson <lilamoln.koffa@gmail.com>
2025-05-25 17:18:03 +0200

    generated projects

---
+++ speculum-backend/src/main/java/com/koffa/speculum_backend/SpeculumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.koffa.speculum_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeculumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeculumBackendApplication.class, args);
+	}
+
+}
---
+++ speculum-backend/src/test/java/com/koffa/speculum_backend/SpeculumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koffa.speculum_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeculumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
